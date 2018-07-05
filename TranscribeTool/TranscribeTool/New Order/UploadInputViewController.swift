7b046b0b2f126013947d23bbcd58baafd0e6c2a3
Ben Scheirman <ben@scheirman.com>
2018-07-05 11:40:35 -0500

    Xcode 10 project changes

---
+++ TranscribeTool/TranscribeTool/New Order/UploadInputViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UploadInputViewController.swift
+//  TranscribeTool
+//
+//  Created by Ben Scheirman on 7/4/18.
+//  Copyright © 2018 NSScreencast. All rights reserved.
+//
+
+import Foundation
